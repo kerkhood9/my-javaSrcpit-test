e1b3a1013050a203f1caafc53bd334c8c3937079
unknown <kerkhood9@gmail.com>
2022-01-25 15:45:55 +0200

    email added

--- home.js
+++ home.js
@@ -1,33 +1,39 @@
-// the password be you please 
-
-var email = document.getElementById("email"); 
-var password = document.getElementById('password');
-var select = document.getElementById('submit');
+const name = document.getElementById("email"); 
+const age = parseInt(document.getElementById('age').value) || 0;
+var list = document.getElementById('people');
+list.innerHTML = '';
 
 
 let people = [
-    { name: 'hardy', age: 10, },
-    { name: 'vusumuzi', age: 21,},
-    { name: 'wandile', age: 65,},
-    { name: 'charity', age: 28,},
-    { name: 'precious', age: 17,},
-    { name: 'andisiwe', age: 90,},
-    { name: 'mmapaseka', age: 30,},
-    { name: 'mhlonipheni', age: 20,},
-    { name: 'khumbulani', age: 30,},
-    { name: 'sophie', age: 18,},
-    { name: 'lucky', age: 32,},
-    { name: 'sithembiso', age: 32,},
-    { name: 'happy', age: 18,},
-    { name: 'selina', age: 26,},
-    { name: 'nkosiphendule', age: 30,},
-
+  { name: 'hardy', age: 10, color: 'blue', hairStyle: 'dread lock' },
+  { name: 'vusumuzi', age: 21, color: 'red', hairStyle: 'chiskop' },
+  { name: 'wandile', age: 65, color: 'blue', hairStyle: 'chiskop' },
+  { name: 'charity', age: 28, color: 'pink', hairStyle: 'afro' },
+  { name: 'precious', age: 17, color: 'pink', hairStyle: 'braid' },
+  { name: 'andisiwe', age: 90, color: 'green', hairStyle: 'afro' },
+  { name: 'mmapaseka', age: 30, color: 'black', hairStyle: 'braid' },
+  { name: 'mhlonipheni', age: 20, color: 'yellow', hairStyle: 'afro' },
+  { name: 'khumbulani', age: 30, color: 'blue', hairStyle: 'afro' },
+  { name: 'sophie', age: 18, color: 'grey', hairStyle: 'braid' },
+  { name: 'lucky', age: 32, color: 'grey', hairStyle: 'chiskop' },
+  { name: 'sithembiso', age: 32, color: 'blue', hairStyle: 'chiskop' },
+  { name: 'happy', age: 18, color: 'white', hairStyle: 'twist' },
+  { name: 'selina', age: 26, color: 'green', hairStyle: 'chiskop' },
+  { name: 'nkosiphendule', age: 30, color: 'white', hairStyle: 'chiskop' },
+  { name: 'thato', age: 30, color: 'green', hairStyle: 'chiskop' },
 ];
 
-for (let i = 0; i < people.length; i++) {
-    if (people[i].name == name) {
-      display.innerHTML += '<li>' + people[i].name + '</li>';
-    }
+for (let count = 0; count < people.length; count++) {
+  if ((people[count]. age > age) && (people[count].age == age )&& (people[count].name=='khumbulani')) {
+    list.innerHTML +=
+      '<li>' +
+      people[count].name +
+      ', age: ' +
+      people[count].age +
+      ', age: ' +
+      people[count].age +
+      '</li>';
   }
-  <script src="./home.html"></script>
+}
+
   
--- login.js
+++ login.js
@@ -1,34 +1,17 @@
-// the email be you please 
+const email = document.getElementById("email"); 
+const password = document.getElementById('password');
+const form = document.getElementById('form');
+const errorElement = document.getElementById('error')
 
-var Email = document.getElementById("email"); 
-var password = document.getElementById('password');
-var color = document.getElementById('color');
-
-
-let people = [
-    { name: 'hardy', age: 10, },
-    { name: 'vusumuzi', age: 21, },
-    { name: 'wandile', age: 65,  },
-    { name: 'charity', age: 28,  },
-    { name: 'precious', age: 17,  },
-    { name: 'andisiwe', age: 90,  },
-    { name: 'mmapaseka', age: 30,  },
-    { name: 'mhlonipheni', age: 20,  },
-    { name: 'khumbulani', age: 30,  },
-    { name: 'sophie', age: 18,  },
-    { name: 'lucky', age: 32,  },
-    { name: 'sithembiso', age: 32,  },
-    { name: 'happy', age: 18, },
-    { name: 'selina', age: 26,  },
-    { name: 'nkosiphendule', age: 30,  },
-
-];
-
-for (let i = 0; i < people.length; i++) {
-    if (people[i].name == name) {
-      display.innerHTML += '<li>' + people[i].name + '</li>';
-    }
+for (let count = 0; count < people.length; count++) {
+  if ((people[count]. email> email) && (people[count].password == password )&& (people[count].name=='khumbulani')) {
+    list.innerHTML +=
+      '<li>' +
+      people[count].email +
+      ', email: ' +
+      people[count].email +
+      ', password: ' +
+      people[count].password +
+      '</li>';
   }
-
-  
-  <script src="./index.html"></script>
+}
