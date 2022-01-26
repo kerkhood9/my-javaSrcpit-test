1a6cb4f92e3bf6335b266fb9975b229835f66071
unknown <kerkhood9@gmail.com>
2022-01-26 14:01:17 +0200

    Name & Age added

--- home.js
+++ home.js
@@ -14,7 +14,7 @@ for (let i = 0; i < names.length; i++) {
   }
 
 for (let count = 0; count < people.length; count++) {
-  if ((people[count]. age > age) && (people[count].age == age )&& (people[count].name=='khumbulani')) {
+  if ((people[count]. name > name ) && (people[count].age == age )&& (people[count].name=='khumbulani')) {
     list.innerHTML +=
       '<li>' +
       people[count].name +
