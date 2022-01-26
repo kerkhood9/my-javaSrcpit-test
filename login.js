834d7c1b6c0ee2f39a2fa1815ebd3c8c088cec0f
unknown <kerkhood9@gmail.com>
2022-01-26 08:22:58 +0200

    email added

--- login.js
+++ login.js
@@ -3,15 +3,73 @@ const password = document.getElementById('password');
 var list = document.getElementById('people');
 list.innerHTML = '';
 
-for (let count = 0; count < people.length; count++) {
-  if ((people[count]. email> email) && (people[count].password == password )&& (people[count].name=='khumbulani')) {
-    list.innerHTML +=
-      '<li>' +
-      people[count].email +
-      ', email: ' +
-      people[count].email +
-      ', password: ' +
-      people[count].password +
-      '</li>';
+// this one get the values
+function getInputValues() {
+  email = document.getElementById("email").value;
+  password = Number(document.getElementById("password").value);
+}
+
+
+function displayTable() {
+  const table = document.getElementById("tableData");
+  table.innerHTML = "";
+
+  for (let i = 0; i < transactions.length; i++) {
+      table.innerHTML +=
+          "<tr><td>" +
+          transactions[i].type +
+          "</td><td>" +
+          transactions[i].detail +
+          "</td><td>" +
+          transactions[i].amount + 
+          "</td></tr>";
+  }
+}
+
+
+// this one will add email
+function addIncome() {
+  getInputValues()
+
+  if (email == "" || email == 0) {
+      alert("Details and amount are required");
+      return false;
+  }
+
+  transactions.push({ type: "email", email: email, password: password });
+  calcutateAndDisplay();
+  displayTable();
+
+}
+// this one will add expense
+function addExpense() {
+  getInputValues()
+
+  if (password == "" || password== 0) {
+      alert("Details and amount are required");
+      return false;
+  }
+
+  transactions.push({ type: "password", password: password, password: password });
+  calcutateAndDisplay();
+  displayTable();
+
+}
+
+function calcutateAndDisplay() {
+  displayemail = document.getElementById("email");
+  displaypassword = document.getElementById("password");
+  display = document.getElementById("display");
+
+ 
+
+  for (let i = 0; i < transactions.length; i++) {
+      if (transactions[i].type == "email") {
+          emailTotal += transactions[i].email;
+      }
+
+      if (transactions[i].type == "password") {
+        passwordTotal += transactions[i].password;
+      }
   }
 }
