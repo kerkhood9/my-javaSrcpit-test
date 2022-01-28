4628332b9249693572c182e3819699f77dd93e15
unknown <kerkhood9@gmail.com>
2022-01-28 09:56:08 +0200

    home.js added

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
-const name = ['hardy','khumbulani','william','Mmapaseka','happy','sithembiso','kagisho','mdud','precious','selina','nkosi','andiwe'];
+const names = ['hardy','khumbulani','william','Mmapaseka','happy','sithembiso','kagisho','mdud','precious','selina','nkosi','andiwe'];
 const age = ['55','30','21','32','22','32','23','29','32','26','28','31'];
 var list = document.getElementById('people');
 list.innerHTML = '';
@@ -7,7 +7,7 @@ list.innerHTML = '';
 let names = ['hardy','khumbulani','william','Mmapaseka','happy','sithembiso','kagisho','mdud','precious','selina','nkosi','andiwe'];
 
 let display = document.getElementById("display");
-const name = form.elements['name'];
+const names = form.elements['name'];
 const age = form.elements['age'];
 
 // getting the element's value
@@ -40,10 +40,10 @@ document.querySelector('button').addEventListener('click', checkAge);
 
 
 for (let count = 0; count < people.length; count++) {
-  if ((people[count]. name > name ) && (people[count].age == age ) && (people[count].name=='khumbulani')) {
+  if ((people[count]. names > names ) && (people[count].age == age ) && (people[count].names=='khumbulani')) {
     list.innerHTML +=
       '<li>' +
-      people[count].name +
+      people[count].names +
       ', name: ' +
       people[count].age +
       ', age: ' +
